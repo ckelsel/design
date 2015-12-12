dab3594b097a3c212f0ef336c85541986ba6ab35
ckelsel <ckelsel@hotmail.com>
2015-12-12 14:16:21 +0800

    pizza problem

---
+++ design/src/com/design/factory/solution1/Pizza.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2015 KunMing Xie <ckelsel@hotmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package com.design.factory.solution1;
+
+public abstract class Pizza {
+	public static Pizza orderPizza(String type) {
+		Pizza pizza = null;
+		
+		if (type.equals("cheese")) {
+			pizza = new CheesePizza();
+		} else if (type.equals("greek")) {
+			pizza = new GreekPizza();
+		} else if (type.equals("clam")) {
+			pizza = new ClamPizza();
+		} else if (type.equals("vegggie")) {
+			pizza = new VeggiePizza();
+		}
+		
+		
+		
+		pizza.prepare();
+		pizza.back();
+		pizza.cut();
+		pizza.box();
+		return pizza;
+	}
+	
+	public abstract void prepare();
+	
+	public abstract void back();
+	
+	public abstract void cut();
+	
+	public abstract void box();
+}
