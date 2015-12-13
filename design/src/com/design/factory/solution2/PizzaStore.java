b643f21d6a5b40f0919d0fa965aae05d063a7682
ckelsel <ckelsel@hotmail.com>
2015-12-13 20:30:29 +0800

    define simple factory (not a true design pattern

---
+++ design/src/com/design/factory/solution2/PizzaStore.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2015 KunMing Xie <ckelsel@hotmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package com.design.factory.solution2;
+
+import com.design.factory.solution1.Pizza;
+
+public class PizzaStore {
+	SimplePizzaFactory mFactory;
+	
+	public PizzaStore(SimplePizzaFactory factory) {
+		mFactory = factory;
+	}
+	
+	Pizza orderPizza(String type) {
+		Pizza pizza;
+		
+		pizza = mFactory.createPizza(type);
+		
+
+		pizza.prepare();
+		pizza.back();
+		pizza.cut();
+		pizza.box();
+		return pizza;
+	}
+}
