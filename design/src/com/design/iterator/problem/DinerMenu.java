429366658ed17754b543bf8bfa17284b6ccd9bc5
ckelsel <ckelsel@hotmail.com>
2015-12-26 20:55:52 +0800

    iterator scene

---
+++ design/src/com/design/iterator/problem/DinerMenu.java
@@ -0,0 +1,61 @@
+/**-------------------------------------------------------------------------
+ * Copyright (C) 2015 KunMing Xie <ckelsel@hotmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package com.design.iterator.problem;
+
+import java.util.ArrayList;
+
+public class DinerMenu {
+	static final int MAX_ITEMS = 6;
+	int numberOfItems = 0;
+	MenuItem[] menuItems;
+  
+	public DinerMenu() {
+		menuItems = new MenuItem[MAX_ITEMS];
+ 
+		addItem("Vegetarian BLT",
+			"(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99);
+		addItem("BLT",
+			"Bacon with lettuce & tomato on whole wheat", false, 2.99);
+		addItem("Soup of the day",
+			"Soup of the day, with a side of potato salad", false, 3.29);
+		addItem("Hotdog",
+			"A hot dog, with saurkraut, relish, onions, topped with cheese",
+			false, 3.05);
+		addItem("Steamed Veggies and Brown Rice",
+			"Steamed vegetables over brown rice", true, 3.99);
+		addItem("Pasta",
+			"Spaghetti with Marinara Sauce, and a slice of sourdough bread",
+			true, 3.89);
+	}
+  
+	public void addItem(String name, String description, 
+	                     boolean vegetarian, double price) 
+	{
+		MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+		if (numberOfItems >= MAX_ITEMS) {
+			System.err.println("Sorry, menu is full!  Can't add item to menu");
+		} else {
+			menuItems[numberOfItems] = menuItem;
+			numberOfItems = numberOfItems + 1;
+		}
+	}
+ 
+	public MenuItem[] getMenuItems() {
+		return menuItems;
+	}
+}
---
+++ design/src/com/design/iterator/problem/MenuItem.java
@@ -0,0 +1,51 @@
+/**-------------------------------------------------------------------------
+ * Copyright (C) 2015 KunMing Xie <ckelsel@hotmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package com.design.iterator.problem;
+
+public class MenuItem {
+	String name;
+	String description;
+	boolean vegetarian;
+	double price;
+	
+	public MenuItem(String name,
+					String description,
+					boolean vegetarian,
+					double price) {
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegetarian;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+}
